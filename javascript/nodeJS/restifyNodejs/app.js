bb92448eb53551c62ca22e3a94af404b94f4bbcf
Lionel <lioneleoy@gmail.com>
2018-07-06 22:16:30 -0400

    node api

--- javascript/nodeJS/restifyNodejs/app.js
+++ javascript/nodeJS/restifyNodejs/app.js
@@ -1,13 +1,24 @@
 var restify = require('restify');
-
-function respond(req, res, next) {
-  res.send('hello ' + req.params.name);
-  next();
-}
-
 var server = restify.createServer();
-server.get('/hello/:name', respond);
-server.head('/hello/:name', respond);
+
+var users = {};
+var max_user_id = 0;
+
+server.get("/", function(req,res,next){
+    res.setHeader('content-type', 'application/json');
+    res.writeHead(200);
+    res.end(JSON.stringify(users));
+    return next();
+});
+
+// function respond(req, res, next) {
+//   res.send('hi ' + req.params.name);
+//   next();
+// }
+
+
+// server.get('/hello/:name', respond);
+// server.head('/hello/:name', respond);
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
