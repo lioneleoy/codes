e814138fb278b90640a7c69692c8c98ccd2f1fc8
Lionel <lionelrajal@Lionels-MacBook-Pro.local>
2018-07-24 22:36:23 -0400

    added git ignore

--- javascript/nodeJS/restifyNodejs/app_old.js
+++ javascript/nodeJS/restifyNodejs/app_old.js
@@ -2,25 +2,7 @@ var restify = require('restify');
 var plugins = require('restify-plugins');
 var server = restify.createServer();
 
-function respond(res, next, status, data, http_code){
 
-var response= {
-    'status': status,
-    'data': data
-};
-res.setHeader('content-type', 'application/json');
-res.writeHead(http_code);
-res.end(JSON.stringify(response));
-return next();
-}
-
-function success(res, next, data){
-    respond(res, next, 'success', data, '200');
-}
-
-function failure(res, next, data, http_code){
-    respond(res, next, 'failure', data, http_code);
-}
 var users = {};
 var max_user_id = 0;
 
@@ -28,12 +10,17 @@ server.use(plugins .acceptParser(server.acceptable));
 server.use(plugins.bodyParser());
 
 server.get("/", function(req,res,next){
-    success(res, next, users);
+    res.setHeader('content-type', 'application/json');
+    res.writeHead(200);
+    res.end(JSON.stringify(users));
+    return next();
 });
 
 server.get("/user/:id", function(req,res,next){
-
-    success(res, next, users[parseInt(req.params.id)]);
+    res.setHeader('content-type', 'application/json');
+    res.writeHead(200);
+    res.end(JSON.stringify(users[parseInt(req.params.id)]));
+    return next();
 });
 
 server.put("/user/:id", function(req,res,next){
@@ -68,6 +55,15 @@ server.del("/user/:id", function(req,res,next){
     return next();
 });
 
+
+// function respond(req, res, next) {
+//   res.send('hi ' + req.params.name);
+//   next();
+// }
+
+
+// server.get('/hello/:name', respond);
+// server.head('/hello/:name', respond);
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
