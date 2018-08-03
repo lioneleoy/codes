fb0eb096abb61ebe4f6aac7112b9c68c79121dcd
Lionel <lioneleoy@gmail.com>
2018-08-02 21:28:18 -0400

    added read me

---
+++ Python/RSSFeed/README.md
@@ -0,0 +1,18 @@
+#Available Fields in the RSS
+updated_parsed
+updated
+published_parsed
+links
+title
+author
+summary_detail
+summary
+content
+guidislink
+title_detail
+link
+authors
+author_detail
+id
+tags
+published
--- Python/RSSFeed/sampleFeedReader.py
+++ Python/RSSFeed/sampleFeedReader.py
@@ -9,12 +9,15 @@ feed = feedparser.parse("https://www.kb.cert.org/vulfeed")
 feed_title = feed['feed']['title']
 feed_entries = feed.entries
 
-for entry in feed.entries:
-    article_title = entry.title
-    article_link = entry.link
-    article_published_at = entry.published # Unicode string
-    article_published_at_parsed = entry.published_parsed # Time object
-    article_author = entry.author
-    print "{}[{}]".format(article_title, article_link)
-    print "Published at {}".format(article_published_at)
-    print "Published by {}".format(article_author)
+# for entry in feed.entries:
+#     article_title = entry.title
+#     article_link = entry.link
+#     article_published_at = entry.published # Unicode string
+#     article_published_at_parsed = entry.published_parsed # Time object
+#     article_author = entry.author
+#     print "{}[{}]".format(article_title, article_link)
+#     print "Published at {}".format(article_published_at)
+#     print "Published by {}".format(article_author)
+
+for entry in feed.entries[0]:
+    print "{}".format(entry)
