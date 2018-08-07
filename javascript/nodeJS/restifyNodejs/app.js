6e5a8927ea455d93c816288b719e5f87dfee547d
Lionel <lioneleoy@gmail.com>
2018-08-06 22:00:41 -0400

    upated gitignore and added mongo connection

--- javascript/nodeJS/restifyNodejs/app.js
+++ javascript/nodeJS/restifyNodejs/app.js
@@ -1,11 +1,13 @@
-
 var restify = require('restify');
 var plugins = require('restify-plugins');
 var validator = require('restify-validator');
 var server = restify.createServer();
 var setupController = require('./controllers/setupController.js');
 var userController = require('./controllers/userController.js');
+var config =require('./config/dbConnection.js'); 
+var mongoose = require('mongoose');
 
+mongoose.connect(config.getMongoConnection() );
 setupController(server, restify, validator);
 userController(server);
 
---
+++ javascript/nodeJS/restifyNodejs/config/dbConnection.js
@@ -0,0 +1,5 @@
+module.exports = {
+    getMongoConnection : function(){
+        return 'mongodb://lionelr:Nopoint@12@ds115738.mlab.com:15738/nodetest';
+    }
+}
